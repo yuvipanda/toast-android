c519a11bad130bb2a0f31d30c018a0fd788f7cdd
Isaac John Wesley <wonderviz@gmail.com>
2013-01-19 23:49:09 +0530

    Added slideview layout with buttons

--- src/in/yuvi/toaster/PlaceActivity.java
+++ src/in/yuvi/toaster/PlaceActivity.java
@@ -11,5 +11,6 @@ public class PlaceActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_listplaces);
+        
     }
 }
