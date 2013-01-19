f654080be1ebb869350cad6b6108faa89a3eb709
YuviPanda <yuvipanda@gmail.com>
2013-01-19 20:51:31 +0530

    Added layouts for menu

--- src/in/yuvi/toaster/PlaceActivity.java
+++ src/in/yuvi/toaster/PlaceActivity.java
@@ -2,8 +2,12 @@ package in.yuvi.toaster;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.MenuItem;
+import com.slidingmenu.lib.SlidingMenu;
+import com.slidingmenu.lib.app.SlidingActivity;
 
-public class PlaceActivity extends Activity {
+public class PlaceActivity extends SlidingActivity {
     /**
      * Called when the activity is first created.
      */
@@ -11,5 +15,23 @@ public class PlaceActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_listplaces);
+        setBehindContentView(R.layout.layout_slidemenu);
+
+        getSlidingMenu().setBehindOffset(160);
+        getSlidingMenu().setMode(SlidingMenu.LEFT);
+        getSlidingMenu().setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setSlidingActionBarEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                toggle();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
